64e7431acdbc1a3a682bec82ee63a96fa1fe2fe6
simocompsci <hamids9alo7777@gmail.com>
2025-11-14 20:46:05 +0100

    added the folders that i will be testing with

---
+++ nonSecure/nonSecure/src/main/java/com/TestApp/nonSecure/NonSecureApplication.java
@@ -0,0 +1,13 @@
+package com.TestApp.nonSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonSecureApplication.class, args);
+	}
+
+}
---
+++ nonSecure/nonSecure/src/test/java/com/TestApp/nonSecure/NonSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestApp.nonSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
